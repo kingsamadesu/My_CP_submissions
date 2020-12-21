2461048672482c348fd5ee040abe8577c848ddfe
Ismail Oukha <55664392+kingsamadesu@users.noreply.github.com>
2020-12-21 23:21:49 +0100

    Create Solution.java

---
+++ java/1551. Minimum Operations to Make Array Equal/Solution.java
@@ -0,0 +1,15 @@
+/*
+Your runtime beats 100.00 % of java submissions.
+Your memory usage beats 99.19 % of java submissions.
+*/
+
+class Solution {
+    public int minOperations(int n) {
+        return n*n/4;
+    }
+}
+
+/*
+Runtime: 0 ms
+Memory Usage: 35.4 MB
+*/
